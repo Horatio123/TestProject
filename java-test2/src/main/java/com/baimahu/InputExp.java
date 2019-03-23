e77b59eb9c4d971ae3b3f6ddbdaeec6f92adafbb
Horatio <alexanderchen1992@gmail.com>
2019-03-23 20:32:57 +0800

    change git name to Horatio

--- java-test2/src/main/java/com/baimahu/InputExp.java
+++ java-test2/src/main/java/com/baimahu/InputExp.java
@@ -11,17 +11,17 @@ public class InputExp {
 //        int i = System.in.read();
 //        System.out.println((char)i);
 
-//        System.out.println("please input a string");
-//        InputStreamReader is = new InputStreamReader(System.in);
-//        BufferedReader br = new BufferedReader(is);
-//        String line = br.readLine();
-//        System.out.println(line);
+        System.out.println("please input a string");
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        String line = br.readLine();
+        System.out.println(line);
 
-          System.out.println("please input a two num");
-          Scanner sc = new Scanner(System.in);
-          int a = sc.nextInt();
-          int b = sc.nextInt();
-          System.out.printf("%d + %d = %d", a, b, a + b);
+//          System.out.println("please input a two num");
+//          Scanner sc = new Scanner(System.in);
+//          int a = sc.nextInt();
+//          int b = sc.nextInt();
+//          System.out.printf("%d + %d = %d", a, b, a + b);
 
 
 
