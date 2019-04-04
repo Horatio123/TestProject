5fd0ff5ebb1ff305acebaca1e990f8d354ce2993
Horatio <alexanderchen1992@gmail.com>
2019-04-04 23:32:00 +0800

    add reflection

---
+++ java-test2/src/main/java/com/baimahu/Reflection/ReflectionTest.java
@@ -0,0 +1,18 @@
+package com.baimahu.Reflection;
+
+import java.lang.reflect.Method;
+
+public class ReflectionTest {
+    public static void main(String[] args) throws Exception{
+        Class c = Class.forName("com.baimahu.Reflection.Test");
+        Test test = (Test)c.newInstance();
+
+        // reflection of private method is fo debug
+        Method method = c.getDeclaredMethod("show");
+        method.setAccessible(true);
+        method.invoke(test);
+
+        System.out.println(c.getSuperclass());
+        System.out.println(c.getDeclaredFields());
+    }
+}
---
+++ java-test2/src/main/java/com/baimahu/Reflection/Test.java
@@ -0,0 +1,7 @@
+package com.baimahu.Reflection;
+
+public class Test {
+    private void show() {
+        System.out.println("test show");
+    }
+}
