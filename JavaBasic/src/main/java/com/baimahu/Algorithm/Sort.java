6100491f631c695962983d1f4deb49f5d7235fcf
Horatio <alexanderchen1992@gmail.com>
2020-05-10 21:46:20 +0800

    quick sort

--- JavaBasic/src/main/java/com/baimahu/Algorithm/Sort.java
+++ JavaBasic/src/main/java/com/baimahu/Algorithm/Sort.java
@@ -3,21 +3,18 @@ package com.baimahu.Algorithm;
 public class Sort {
     public static void main(String[] args) {
         int[] array = {3, 8, 1, 9, 2};
-        BubbleSort bubbleSort = new BubbleSort();
-        int[] arrRes = bubbleSort.sort(array);
-
-        for (int num : arrRes) {
+        //bubbleSort(array);
+        quickSort(array);
+        for (int num : array) {
             System.out.println(num + " ");
         }
     }
-
-}
-class BubbleSort{
-
-    public BubbleSort() {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-
-    public int[] sort(int[] arr) {
+    public static void bubbleSort(int[] arr) {
         boolean sorted;
 
         int temp;
@@ -32,7 +29,36 @@ class BubbleSort{
                 }
             }
         } while (!sorted);
+    }
 
-        return arr;
+    public static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int pivot = arr[(low + high) / 2];
+        int index = partition(arr, low, high, pivot);
+        quickSort(arr, low, index - 1 );
+        quickSort(arr, index, high);
+    }
+
+    private static int partition(int[] arr, int low, int high, int pivot) {
+        while (low <= high) {
+            while (arr[low] < pivot) {
+                low ++;
+            }
+            while (arr[high] > pivot) {
+                high --;
+            }
+            if (low <= high) {
+                swap(arr, low, high);
+                low ++;
+                high --;
+            }
+
+        }
+        return low;
     }
 }
